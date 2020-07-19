fb0cc73ca9f43f7fb10564c9a143ffc046217235
basitsattar <basitsattar850@gmail.com>
2020-07-19 11:43:06 +0500

    fix nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './Components/navbar';
+import Login from './Components/login';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+      <Switch>
+        <Route path={'/'} Component={Login} />
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/Components/login/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    redner() {
+        return (
+            <div>
+                <h1>My details </h1>
+            </div>
+        );
+    }
+}
+
+export default Login;
